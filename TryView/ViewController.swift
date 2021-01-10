02943e65c8c87450edd069402d27ff58e73572e7
FredFu <fjw08199999@gmail.com>
2021-01-10 12:45:39 +0800

    Initial Commit

---
+++ TryView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryView
+//
+//  Created by fred fu on 2021/1/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
